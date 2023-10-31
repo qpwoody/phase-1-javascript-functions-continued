e1f5e1bb1e414f16c2514c85b499553697610a68
quinn woodhouse <qpwoodhouse@gmail.com>
2023-10-31 09:53:02 -0600

    functions cont lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(_activity = "go to the office"){
+    return `This Monday, I will ${_activity}.`
+}
+const mondayActivity = mondayWork("work from home");
+console.log(mondayActivity)
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
+  const highlight = wrapAdjective(`*`)
+console.log(highlight(`a hard worker`))
